b8ddc187301f71e5926cc276a60d1cb5a1f81f45
paperstreets <naviholod@gmail.com>
2023-11-20 16:16:41 +0200

    Add tableau viz to home page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements AfterViewInit {
 
-  constructor() {}
+  constructor() { }
 
+  ngAfterViewInit() {
+    this.loadScript('https://public.tableau.com/javascripts/api/viz_v1.js');
+  }
+
+  loadScript(url: string) {
+    const body = <HTMLDivElement> document.body;
+    const script = document.createElement('script');
+    script.innerHTML = '';
+    script.src = url;
+    script.async = false;
+    script.defer = true;
+    body.appendChild(script);
+  }
 }
