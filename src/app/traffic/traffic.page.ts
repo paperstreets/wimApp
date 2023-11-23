73115846c60b27f54f0cb607d074c7d3c7dd1a40
paperstreets <naviholod@gmail.com>
2023-11-23 17:57:51 +0200

    Add dashboards to home page

--- src/app/traffic/traffic.page.ts
+++ src/app/traffic/traffic.page.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-traffic',
   templateUrl: './traffic.page.html',
   styleUrls: ['./traffic.page.scss'],
 })
-export class TrafficPage implements OnInit {
+
+export class TrafficPage implements AfterViewInit {
 
   constructor() { }
 
-  ngOnInit() {
+  ngAfterViewInit() {
+    this.loadScript('https://public.tableau.com/javascripts/api/viz_v1.js');
   }
 
+  loadScript(url: string) {
+    const body = <HTMLDivElement> document.body;
+    const script = document.createElement('script');
+    script.innerHTML = '';
+    script.src = url;
+    script.async = false;
+    script.defer = true;
+    body.appendChild(script);
+  }
 }
