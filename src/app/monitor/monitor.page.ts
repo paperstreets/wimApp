5a2c646b043cbc825bf4735fe3c47af8975b161f
paperstreets <naviholod@gmail.com>
2023-11-24 15:32:58 +0200

    Add monitoring dashboard

--- src/app/monitor/monitor.page.ts
+++ src/app/monitor/monitor.page.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-monitor',
   templateUrl: './monitor.page.html',
   styleUrls: ['./monitor.page.scss'],
 })
-export class MonitorPage implements OnInit {
+export class MonitorPage implements AfterViewInit {
 
   constructor() { }
 
-  ngOnInit() {
+  ngAfterViewInit() {
+    this.loadScript('https://public.tableau.com/javascripts/api/viz_v1.js');
+  }
+
+  loadScript(url: string) {
+    const body = <HTMLDivElement> document.body;
+    const script = document.createElement('script');
+    script.innerHTML = '';
+    script.src = url;
+    script.async = false;
+    script.defer = true;
+    body.appendChild(script);
   }
 
 }
